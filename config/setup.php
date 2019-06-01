5c89ee17da6c8f1e2ae8b6bbcf8bc0486a01108a
Junior Santos <jsantos.class@gmail.com>
2019-06-01 13:58:54 -0300

    Integração parcial com Vue

---
+++ config/setup.php
@@ -0,0 +1,13 @@
+<?php
+
+
+wp_enqueue_script( 'sd-view', SD_PATH . 'views/libs/vue.js' );
+add_action('admin_menu', 'super_dashboard_page_setup');
+
+function super_dashboard_page_setup(){
+  add_menu_page( 'Super Dashboard', 'Super Dashboard', 'manage_options', 'super-dashboard', 'page_resource', 'dashicons-image-filter', 3 );
+}
+
+function page_resource(){
+  echo file_get_contents( SD_PATH . 'frontend/dist/index.html' );
+}
---
+++ super-dashboard.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Super Dashboard
+  Plugin URI: https://github.com/JJS4ntos
+  Description: Gerencie todos os seus leads e tenha o perfil de cada um deles.
+  Version: ALPHA
+  Author: Jair Júnior
+  Author URI: https://github.com/JJS4ntos
+*/
+define('SD_PATH', plugin_dir_url( __FILE__ ));
+require_once 'config/setup.php';
